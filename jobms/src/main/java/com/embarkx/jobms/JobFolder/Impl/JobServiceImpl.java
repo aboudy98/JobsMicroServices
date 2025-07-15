3d56088b770b712cc9f905845192f9d5f36fd90a
Abdallah Mazloum <abdullah.mazloum1998@gmail.com>
2025-07-15 18:01:58 +0300

    integration zipkin and micrometer

--- jobms/src/main/java/com/embarkx/jobms/JobFolder/Impl/JobServiceImpl.java
+++ jobms/src/main/java/com/embarkx/jobms/JobFolder/Impl/JobServiceImpl.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 import java.util.stream.Collectors;
 
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
 
 import lombok.RequiredArgsConstructor;
 
